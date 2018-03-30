ed79392d441e80b62c3f5eb80bb539a9da85e4aa
Vladimir Malyarevich <vladimir.low@gmail.com>
2018-03-30 11:14:10 -0700

    Release 0.2:
    - stores search reqs and loads it as datalists;
    
    - created common functions;
    
    - added more strict permissions;

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
+// Gets data of specified type from storage
+function getSavedData(type, callback) {
+  chrome.storage.local.get([type], (items) => {
+    callback(chrome.runtime.lastError ? null : items[type]);
+  });
+}
+
+// Adds band or song name to proper list
+function saveData(key, value) {
+  if (value.length === 0) {
+    return;
+  }
+  getSavedData(key, (list) => {
+    if (!list) {
+      var list = [value];
+    } else if (!list.includes(value)) {
+      list.push(value);
+    }
+  var items = {};
+  items[key] = list;
+  console.log(key + ': ' + items[key]);
+  chrome.storage.local.set(items);
+  });
+}
+
 function openAmdm(band, song) {
-  var base_url = "";
-  if (band) {
-    chrome.tabs.update({url: 'https://amdm.ru/akkordi/'+band});
-  };
+  chrome.tabs.update({url: 'https://amdm.ru/akkordi/'+band});
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
       chrome.tabs.executeScript(tabId, {file: 'jquery-1.8.3.min.js'});
@@ -11,16 +33,43 @@ function openAmdm(band, song) {
   });
 };
 
-console.log('Reached popup.js')
 document.addEventListener('DOMContentLoaded', () => {
-  var band = document.getElementById('band');
-  var song = document.getElementById('song');
-  var button = document.getElementsByTagName('button')[0];
+  //chrome.storage.local.clear(); // Flushes data store
+  // Fills up datalists
+  getSavedData('bands', (savedBands) => {
+    if (savedBands) {
+      var bList = document.getElementById('bandList');
+      savedBands.forEach(function(item) {
+        var option = document.createElement('option');
+        option.value = item;
+        bList.appendChild(option)
+      });
+    }
+  });
+  getSavedData('songs', (savedSongs) => {
+    if (savedSongs) {
+      var sList = document.getElementById('songList');
+      savedSongs.forEach(function(item) {
+        var option = document.createElement('option');
+        option.value = item;
+        sList.appendChild(option)
+      });
+    }
+  });
 
+  var button = document.getElementsByTagName('button')[0];
   button.addEventListener('click', () => {
-    //Store vars to use them in content script
-    chrome.storage.local.set({'band': band.value});
-    chrome.storage.local.set({'song': song.value});
-    openAmdm(band.value, song.value);
+    var band = document.getElementById('band');
+    var song = document.getElementById('song');
+    if (!band.value) {
+      alert('Band is not entered');
+      return;
+    } else {
+      saveData('bands', band.value);
+      saveData('songs', song.value);
+      //Stores song to search for it in content script
+      chrome.storage.local.set({'song': song.value});
+      openAmdm(band.value, song.value);
+    }
   });
 });
--- sorter.js
+++ sorter.js
@@ -1,22 +1,24 @@
-// inject script to the page due to limitations of chrome content scripts
+// Inject script to the page due to limitations of chrome content scripts
 // Read more at https://developer.chrome.com/extensions/content_scripts
 var injectScript = document.createElement('script');
 injectScript.textContent = '$(".tablesorter-header-inner:eq(2)").click();';
 document.body.appendChild(injectScript);
-console.log("Sorted!");
+console.log("Songs are sorted!");
 
 // Filter song on the band's page
 function songHighlight() {
   chrome.storage.local.get(['song'], function(storage) {
-    console.log('Searching for song ' + storage.song);
-    var result = $('a').filter(function() {
-      return $(this).text().toLowerCase().indexOf(storage.song.toLowerCase()) >= 0;
-    }).first();
-    if (0 === result.text().length) {
-      alert('WARNING: No song matching "' + storage.song + '" found!');
-    } else {
-      result.css('background-color', 'red');
-      $('html, body').animate({scrollTop: parseInt(result.offset().top)}, 2000);
+    if (storage.song) {
+      console.log('Searching for song ' + storage.song);
+      var result = $('a').filter(function() {
+        return $(this).text().toLowerCase().indexOf(storage.song.toLowerCase()) >= 0;
+      }).first();
+      if (0 === result.text().length) {
+        alert('WARNING: No song matching "' + storage.song + '" found!');
+      } else {
+        result.css('background-color', 'red');
+        $('html, body').animate({scrollTop: parseInt(result.offset().top)}, 2000);
+      }
     }
   });
 }
